68feb01ac7639c9c4ac9cb9c4aae204630f16bee
Kosala YT <158541231+Kshithijaya@users.noreply.github.com>
2025-05-10 13:29:49 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,6 +4,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Session id",
+SESSION_ID: process.env.SESSION_ID || "𝐋ᴀᴋᴀ-𝐌ᴅ=I0pxhCIS#HfCEn2D3F630oP3mPdej-vwk7_bOuVspC1qNipVpDmk
+",
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
